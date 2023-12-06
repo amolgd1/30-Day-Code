10b7589a80ba641f6c9a44d3eaedaf011a554d58
amolgd1 <amolgend94@gmail.com>
2023-12-06 11:29:33 +0530

    Day3/30

--- WithdrawFunds.sol
+++ WithdrawFunds.sol
@@ -5,33 +5,21 @@
 pragma solidity ^0.8.0;
 
 contract Withdrawfund {
-    address owner;
 
-    constructor() {
-        // Constructor sets the contract owner to the address that deploys the contract
-        owner = msg.sender;
+    mapping(address=>uint) public balanceOf;
+
+    function deposit() public payable{
+        balanceOf[msg.sender] += msg.value;
     }
 
-    modifier OnlyOwner() {
-        // Modifier to restrict access to certain functions to the contract owner only
-        require(owner == msg.sender, "Only the owner can call this function");
-        _;
+    function withdraw(address user,uint amount) public{
+        amount = amount * 1 ether;
+        require(balanceOf[msg.sender] >= amount,"You dont have funds to withdraw");
+        require(amount > 0,"Amount must be greater than 0");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[user] += amount; 
+        payable(user).transfer(amount);
     }
 
-    function deposit()  public payable
-    {
-        // This function allows anyone to deposit funds into the contract.
-        // It's marked as payable to accept incoming Ether transactions.
-    }
-
-    function withdraw(address user, uint256 amount) public payable OnlyOwner {
-        // This function allows the contract owner to withdraw Ether from the contract 
-        // and send it to the specified 'user' address.
-        payable(user).transfer(amount * 1 ether);
-    }
-
-    function balance(address user) public view returns (uint256) {
-        // This function allows anyone to query the balance of the 'user' address.
-        return address(user).balance;
-    }
 }
