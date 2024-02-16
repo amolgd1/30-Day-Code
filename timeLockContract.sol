e3db4c31a440b0c4f7f13038b0365590f1011f80
unknown <amolgd94@gmail.com>
2024-02-16 23:02:24 +0530

    Day4/30

--- timeLockContract.sol
+++ timeLockContract.sol
@@ -3,47 +3,63 @@
 //// **** Write a Solidity function to implement a time-locked contract,
 //  which allows funds to be withdrawn only after a certain time has elapsed. ****////
 
-pragma solidity >0.5.0 <0.9.0;
+pragma solidity 0.8.19;
 
-contract timeLock {
-    uint256 public unlocktime; // Timestamp when funds can be unlocked
-
-    mapping(address=>uint) public balanceOf;
-
-    constructor(uint256 unlocktimeMinutes) {
-        unlocktime = block.timestamp + (unlocktimeMinutes * 1 minutes); // Calculate the unlock time
+contract TimeLock {
+    struct Deposit {
+        uint256 unlockTime;
+        uint256 amount;
     }
 
-    // Modifier to restrict a function to be callable only after the unlock time
-    modifier Afterunlocktime() {
+    mapping(address => Deposit) private deposits;
+
+    modifier AfterUnlockTime() {
         require(
-            block.timestamp >= unlocktime,
+            afterUnlockTime(),
             "Funds cannot be withdrawn before the unlock time"
         );
         _;
     }
 
-    // Function to allow deposits to the contract (can receive Ether)
-    function deposit() public payable {
-        balanceOf[msg.sender] += msg.value;
+    function afterUnlockTime() internal view returns (bool) {
+        uint256 unlockTime = deposits[msg.sender].unlockTime;
+        return block.timestamp >= unlockTime;
     }
 
-    function setNewUnlocktime(uint _newUnlocktimeInMinutes) public {
-        uint newUnlockTimestamp = block.timestamp + (_newUnlocktimeInMinutes * 1 minutes);
-        require(newUnlockTimestamp > unlocktime,"Unlocktime must begreater than last unlock time");
-        unlocktime = newUnlockTimestamp;
+    function deposit(uint256 _unlockTimeInMinutes) external payable {
+        uint256 unlockTime = block.timestamp +
+            (_unlockTimeInMinutes * 1 minutes);
+        require(msg.value != 0, "deposit failed, must amount != 0");
+        require(
+            unlockTime > deposits[msg.sender].unlockTime,
+            "New unlock time must be greater than the original unlock time"
+        );
+
+        deposits[msg.sender].unlockTime = unlockTime;
+        deposits[msg.sender].amount = msg.value;
     }
 
-    // Function to withdraw funds from the contract, restricted to the owner and after unlock time
-    function withdraw(address user, uint256 amount) public payable Afterunlocktime
-    {
-        require(balanceOf[msg.sender] >= amount * 1 ether,"You dont have funds to withdraw");
-        balanceOf[msg.sender] -= amount * 1 ether;
-        payable(user).transfer(amount * 1 ether); // Transfer the funds to the accounts
+    function setUnlockTime(uint256 _newUnlockTimeInMinutes) external {
+        uint256 newUnlockTime = block.timestamp +
+            (_newUnlockTimeInMinutes * 1 minutes);
+        require(
+            newUnlockTime > deposits[msg.sender].unlockTime,
+            "unlock time must be greater than last unlock time"
+        );
+
+        deposits[msg.sender].unlockTime = newUnlockTime;
     }
 
-    // Function to check if the unlock time has passed
-    function Isunlocktime() public view returns (bool) {
-        return block.timestamp >= unlocktime;
+    function withdraw() public payable AfterUnlockTime {
+        require(
+            deposits[msg.sender].amount >= msg.value,
+            "you dnt have funds to withdraw"
+        );
+
+        deposits[msg.sender].unlockTime = 0;
+        deposits[msg.sender].amount -= msg.value;
+
+        (bool success, ) = payable(msg.sender).call{value: msg.value}("");
+        require(success, "withdraw failed");
     }
 }
